c573f555cc965214b0e38481aea545d9a906067c
Shams, Ahmad <ahmad_shams@bmc.com>
2020-07-27 02:29:15 +0300

    init version

---
+++ adder/src/main/java/com/dzone/albanoj2/maven/java/App.java
@@ -0,0 +1,14 @@
+package com.dzone.albanoj2.maven.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Adder adder = new Adder();
+        System.out.println("2 + 2 = " + adder.add(2, 2));
+    }
+}
