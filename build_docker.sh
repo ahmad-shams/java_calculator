ea28e6342930bbe790408f427b148693542ff9fe
Shams, Ahmad <ahmad_shams@bmc.com>
2020-07-31 01:59:58 +0300

    Remove debug env

--- build_docker.sh
+++ build_docker.sh
@@ -5,7 +5,7 @@ if [ -z "$TRAVIS_BUILD_DIR" ]
 then
       cd $TRAVIS_BUILD_DIR
 fi
-env
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t $DOCKER_IMAGE .
 docker push $DOCKER_IMAGE
