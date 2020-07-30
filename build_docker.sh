a3b786675231dfc41eb20359a5c7847bb34b6ba5
Shams, Ahmad <ahmad_shams@bmc.com>
2020-07-31 01:33:42 +0300

    add deploy as stage

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+
+if [ -z "$TRAVIS_BUILD_DIR" ]
+then
+      cd $TRAVIS_BUILD_DIR
+fi
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-cd $HOME
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
 
